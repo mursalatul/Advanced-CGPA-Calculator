d02b6b08b6aa0a2bbb10ab20128c187e14047504
mursalatul <mursalatulislampallob@gmail.com>
2023-07-26 23:32:13 +0600

    added Database class for handling mysql data

--- src/advancedcgpacalculator/AdvancedCGPACalculator.java
+++ src/advancedcgpacalculator/AdvancedCGPACalculator.java
@@ -8,32 +8,10 @@ package advancedcgpacalculator;
  *
  * @author mursalatul
  */
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import test.Test1;
 public class AdvancedCGPACalculator {
+
     public static void main(String[] args) {
-        // Database credentials
-        String url = "jdbc:mysql://localhost:3306/cgpa_storage";
-        String user = "root";
-        String password = "root";
-
-        // Establish a connection
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the database!");
-
-            // Now you can execute queries, update data, etc.
-            // For example:
-            // Statement statement = connection.createStatement();
-            // ResultSet resultSet = statement.executeQuery("SELECT * FROM your_table_name;");
-            // while (resultSet.next()) {
-            //     // Process the data
-            // }
-
-        } catch (SQLException e) {
-            System.err.println("Error connecting to the database: " + e.getMessage());
-        }
+        Test1 t = new Test1();
     }
 }
---
+++ src/database/Database.java
@@ -0,0 +1,143 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+import gradecalculation.Grade;
+/**
+ *
+ * @author mursalatul
+ */
+public class Database {
+
+    private Connection connection;
+    private Statement statement;
+
+    /**
+     * connect the database and initialize the statement
+     * 
+     * @param none
+     * @return none
+    */
+    public Database() {
+        // Database credentials
+        String url = "jdbc:mysql://localhost:3306/cgpa_storage";
+        String user = "root";
+        String password = "root";
+
+        // Establish a connection
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            System.out.println("Connected to the database!");
+
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+        }
+    }
+
+    /**
+     * generate table name from semester number
+     * 
+     * @param semester number
+     * @return sql table name
+     */
+    private String tableName(int semester_no) {
+        return "semester" + Integer.toString(semester_no);
+    }
+
+    /**
+     * execute a mysql command
+     * 
+     * @param string (command)
+     * @return Successful / error message
+     */
+    private String execute(String command) {
+        String status;
+        //        executing the command with this.statement
+        try {
+            this.statement.execute(command);
+            status = "Successful";
+        } catch (SQLException e) {
+            status = e.getMessage();
+        }
+        return status;
+    }
+
+    /**
+     * create table with appropriate columns(No, Course No, Course Title,
+     * Incourse, Final, Grade Letter, Grade Point, Avg Grade Letter, Avg Grade
+     * Point)
+     *
+     * @param semester number
+     * @return Successful / error message
+     */
+    public String createTable(int semester_no) {
+        String status;
+//        setting up table name. table name will be "semester"+number of semester
+        String tableName = tableName(semester_no);
+
+//        creating mysql command
+        String createTableCommand = "CREATE TABLE `cgpa_storage`." + tableName + " (`No` INT(1) NOT NULL AUTO_INCREMENT , `Course No` TEXT NOT NULL , `Course Title` TEXT NOT NULL , `Incourse` INT NOT NULL , `Final` INT NOT NULL , `Grade Letter` TEXT NOT NULL , `Grade Point` FLOAT NOT NULL , `Avg Grade Letter` TEXT NOT NULL , `Avg Grade Point` FLOAT NOT NULL , PRIMARY KEY (`No`)) ENGINE = InnoDB";
+
+//        executing the command with this.statement
+        try {
+            this.statement.execute(createTableCommand);
+            status = "Successful";
+        } catch (SQLException e) {
+            status = e.getMessage();
+        }
+        return status;
+    }
+
+    /**
+     * delete full table
+     *
+     * @param semester numberr
+     * @return true / error message
+     */
+    public String deleteTable(int semester_no) {
+        String status;
+//        setting up table name. table name will be "semester"+number of semester
+        String tableName = tableName(semester_no);
+
+//        creating mysql command
+        String createTableCommand = "DROP TABLE " + tableName;
+
+//        executing the command with this.statement
+        try {
+            this.statement.execute(createTableCommand);
+            status = "Successful";
+        } catch (SQLException e) {
+            status = e.getMessage();
+        }
+        return status;
+    }
+
+    public String insertData(int semester_no, String course_no, String course_title, int inc, int fin) {
+        String table = tableName(semester_no);
+        String command;
+        Grade grd = new Grade();
+        grd.setMark((inc), fin, 0);
+        String grade_letter = grd.gradeLetter();
+        float grade_point = grd.gradePoint();
+        command = "INSERT INTO " + table + " (`Course No`, `Course Title`, `Incourse`, `Final`, `Grade Letter`, `Grade Point`, `Avg Grade Letter`, `Avg Grade Point`) " +
+                "VALUES " + "('" + course_no + "','" + course_title + "','" + Integer.toString(inc) + 
+                "','"+ Integer.toString(fin) + "','" + grade_letter+"','"+grade_point+"', 'NULL', '0.00')";
+//        System.out.println(command);
+        return execute(command);
+    }
+    
+    /**
+     * clear all the data of a table
+     * 
+     * @param semester number
+     * @return true / error message
+    */
+    public String clearTable(int semester_no) {
+        String tableName = tableName(semester_no);
+        return execute("DELETE FROM " + tableName);
+    }
+}
